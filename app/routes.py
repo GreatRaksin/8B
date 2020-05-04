360a3adee8beb002b5c5828f45d93729e1f66ad0
Architect <greatraksin@icloud.com>
2020-05-04 08:58:06 +0300

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import App
+
+# из папки app импортирую экземпляр класса Flask по имени App
+
+
+@App.route('/')
+def index():
+    words = ['привет', 'пока', 'я очень буду ждать звонка', 'полетели', 'hello']
+    '''
+        я связываю МЕТКУ words на странице index.html
+        со своим массивом words, в котором лежат слова
+    '''
+    return render_template('index.html', words=words)
