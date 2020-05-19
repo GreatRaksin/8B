16290f8e175a960ac788f5d9dc3bcf56041f9a07
Architect <greatraksin@icloud.com>
2020-05-19 11:39:11 +0300

    Adding model for User table

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 
 
 class User(db.Model):
+    """ описываю таблицу, в которой будут храниться данные пользователей """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
