1a282330dbc009e915fa956faad33efb59ef20ea
Architect <greatraksin@icloud.com>
2020-05-26 12:08:14 +0300

    Adding login/logout function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, redirect, flash, url_for
+from flask_login import current_user, login_user, logout_user
+
 from app import App
 from app.forms import LoginForm
+from app.models import User
+
 
 # из папки app импортирую экземпляр класса Flask по имени App
 
@@ -18,5 +22,23 @@ def index():
 
 @App.route('/sign_in', methods=['GET', 'POST'])
 def signIn():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.checkPassword(form.password.data):
+            flash('Неправильный логин и/или пароль')
+            return redirect(url_for('signIn'))
+        login_user(user, remember=form.remember.data)
+        return redirect(url_for('index'))
+
     return render_template('login.html', form=form)
+
+
+@App.route('/sign_out')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
